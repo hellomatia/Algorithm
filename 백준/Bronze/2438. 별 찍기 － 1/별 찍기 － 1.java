aeadb17fcdb7f0826c275e36ed70d9c6ed680304
hellomatia <101452054+hellomatia@users.noreply.github.com>
2023-04-23 20:15:27 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 132 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<T; i++) {
+            for (int j=0; j<=i; j++){
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        br.close();
+
+        bw.flush();
+        bw.close();
+
+    }
+}
+ 
