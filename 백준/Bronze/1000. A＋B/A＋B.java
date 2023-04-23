e5b5fa0e6a5a97e50899870ade9d28c460cb5a91
hellomatia <101452054+hellomatia@users.noreply.github.com>
2023-04-23 20:09:29 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a, b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+
+        System.out.println(a+b);
+
+    }
+}
