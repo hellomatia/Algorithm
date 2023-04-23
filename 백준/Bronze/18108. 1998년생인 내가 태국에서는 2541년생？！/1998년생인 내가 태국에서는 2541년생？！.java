2ffb3e6f1c0d7416ad4d71252a3305aea41cab66
hellomatia <101452054+hellomatia@users.noreply.github.com>
2023-04-23 20:10:37 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        year = scanner.nextInt();
+
+        System.out.println(year-543);
+
+    }
+}
