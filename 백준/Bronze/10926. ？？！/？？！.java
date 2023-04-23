c113094feaa4b6bd3fb4a13b1a5c67cd6c622b39
hellomatia <101452054+hellomatia@users.noreply.github.com>
2023-04-23 20:10:24 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String id;
+        id = scanner.nextLine();
+
+        System.out.println(id+"??!");
+        
+    }
+}
