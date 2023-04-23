d036504fb7bc6b38697a8f373f22b1435b62ce5c
hellomatia <101452054+hellomatia@users.noreply.github.com>
2023-04-23 20:11:10 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        long firstNum = scanner.nextLong();
+        long secondNum = scanner.nextLong();
+        long thirdNum = scanner.nextLong();
+
+
+        System.out.println(firstNum+secondNum+thirdNum);
+
+    }
+}
