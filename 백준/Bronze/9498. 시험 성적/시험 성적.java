5158c8782ec7f91f9c78e38dfef0dac17536fa02
hellomatia <101452054+hellomatia@users.noreply.github.com>
2023-04-23 20:13:06 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int score = scanner.nextInt();
+
+        if (90<=score && score<=100) {
+            System.out.println("A");
+        }
+        else if (80<=score && score<=89) {
+            System.out.println("B");
+        }
+        else if (70<=score && score<=79) {
+            System.out.println("C");
+        }
+        else if (60<=score && score<=69) {
+            System.out.println("D");
+        }
+        else {
+            System.out.println("F");
+        }
+
+    }
+}
