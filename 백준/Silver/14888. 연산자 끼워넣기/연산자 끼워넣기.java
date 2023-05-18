22ead45f92f7eb6549610a12ad15728fb5f4cc5a
hellomatia <101452054+hellomatia@users.noreply.github.com>
2023-05-18 17:15:25 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 132 ms, Memory: 15848 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -5,7 +5,7 @@ public class Main {
 
     int[] num;
     int numCount;
-    int[] modify;
+    int[] operator;
     int minNum = Integer.MAX_VALUE;
     int maxNum = Integer.MIN_VALUE;
 
@@ -16,7 +16,7 @@ public class Main {
 
         numCount = Integer.parseInt(bf.readLine());
         num = new int[numCount];
-        modify = new int[4];
+        operator = new int[4];
 
         StringTokenizer st = new StringTokenizer(bf.readLine());
         for(int i=0; i<numCount; i++) {
@@ -25,10 +25,10 @@ public class Main {
 
         st = new StringTokenizer(bf.readLine());
         for(int i=0; i<4; i++) {
-            modify[i] = Integer.parseInt(st.nextToken());
+            operator[i] = Integer.parseInt(st.nextToken());
         }
 
-        calculation(modify, num[0], 1);
+        dfs(num[0],1);
 
 
         bw.write(String.valueOf(maxNum));
@@ -40,8 +40,8 @@ public class Main {
         bw.close();
     }
 
-    public void calculation(int[]mod, int number, int depth) {
-        if(numCount==depth) {
+    public void dfs(int number, int index) {
+        if(numCount==index) {
             if(number>maxNum) {
                 maxNum = number;
             }
@@ -53,32 +53,26 @@ public class Main {
         }
 
         for(int i=0; i<4; i++) {
-            if(mod[i]==0) continue;
-            if(i==0) {
-                int result;
-                result = number + num[depth];
-                mod[i]--;
-                calculation(mod, result, depth+1);
-                mod[i]++;
-            } else if(i==1) {
-                int result;
-                result = number - num[depth];
-                mod[i]--;
-                calculation(mod, result, depth+1);
-                mod[i]++;
-            } else if(i==2) {
-                int result;
-                result = number * num[depth];
-                mod[i]--;
-                calculation(mod, result, depth+1);
-                mod[i]++;
-            } else {
-                int result;
-                result = number / num[depth];
-                mod[i]--;
-                calculation(mod, result, depth+1);
-                mod[i]++;
+            if(operator[i]==0) continue;
+
+            operator[i]--;
+
+            switch (i) {
+                case 0:
+                    dfs(number + num[index], index+1);
+                    break;
+                case 1:
+                    dfs(number - num[index], index+1);
+                    break;
+                case 2:
+                    dfs(number * num[index], index+1);
+                    break;
+                case 3:
+                    dfs(number / num[index], index+1);
+                    break;
             }
+
+            operator[i]++;
         }
     }
 
