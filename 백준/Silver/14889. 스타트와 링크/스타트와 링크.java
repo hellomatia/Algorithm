c55679184d309f4992ca81473bb0572d80f82c93
hellomatia <101452054+hellomatia@users.noreply.github.com>
2023-05-19 16:23:39 +0900

    [Silver II] Title: 스타트와 링크, Time: 444 ms, Memory: 25984 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    int[][] superPower;
+    int[][] team;
+    int n;
+    int teamCount = 0;
+    boolean[] hasTeam;
+    int minPowerGap = Integer.MAX_VALUE;
+
+    public void solution() throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(bf.readLine());
+
+        superPower = new int[n][n];
+        team = new int[2][n/2];
+        hasTeam = new boolean[n];
+
+
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            for(int j=0; j<n; j++) {
+                superPower[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        splitTeam(0);
+
+
+        bw.write(String.valueOf(minPowerGap));
+
+
+        bw.flush();
+        bw.close();
+    }
+
+    public void splitTeam(int start) {
+
+        if(teamCount==n/2) {
+            calculatorTeamPower();
+            return;
+        }
+
+        for(int i = start; i<n; i++) {
+            if(!hasTeam[i]){
+                teamCount++;
+                hasTeam[i] = true;
+                splitTeam(i+1);
+                teamCount--;
+                hasTeam[i] = false;
+            }
+        }
+    }
+
+    public void calculatorTeamPower() {
+        int[] teamPower = new int[2];
+
+        for(int i=0; i<n-1; i++) {
+            for(int j=i+1; j<n; j++) {
+                if(hasTeam[i]==true&&hasTeam[j]==true) {
+                    teamPower[0] += superPower[i][j];
+                    teamPower[0] += superPower[j][i];
+                }
+                if(hasTeam[i]==false&&hasTeam[j]==false) {
+                    teamPower[1] += superPower[i][j];
+                    teamPower[1] += superPower[j][i];
+                }
+            }
+        }
+
+        int powerGap = Math.abs(teamPower[0]-teamPower[1]);
+
+        minPowerGap = Math.min(minPowerGap, powerGap);
+    }
+
+
+    public static void main (String[] args) throws IOException {
+        new Main().solution();
+    }
+
+
+}
