7209d2d758e60f1adf4cac94b40d27e80433a3ed
hellomatia <101452054+hellomatia@users.noreply.github.com>
2023-05-22 21:45:26 +0900

    [Gold II] Title: 2048 (Easy), Time: 324 ms, Memory: 35496 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -13,17 +13,15 @@ public class Main {
         N = Integer.parseInt(bf.readLine()); // 가로, 세로
 
         int[][] gameMap = new int[N][N];
-        boolean[][] hasNum = new boolean[N][N];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             for (int j = 0; j < N; j++) {
                 gameMap[i][j] = Integer.parseInt(st.nextToken());
-                if (gameMap[i][j] != 0) hasNum[i][j] = true;
             }
         }
 
-        playGame(gameMap, hasNum, 0);
+        playGame(gameMap,0);
 
 
         bw.write(maxNum + "\n");
@@ -32,166 +30,138 @@ public class Main {
         bw.flush();
         bw.close();
     }
-    public void playGame(int[][] gameMap, boolean[][] hasNum, int count) {
+    public void playGame(int[][] gameMap, int count) {
 
         if (count == 5) {
-            calculationScore(gameMap, hasNum);
+            calculationScore(gameMap);
             return;
         }
 
-        playGameUp(gameMap, hasNum, count);
-        playGameDown(gameMap, hasNum, count);
-        playGameLeft(gameMap, hasNum, count);
-        playGameRight(gameMap, hasNum, count);
+        playGameUp(gameMap, count);
+        playGameDown(gameMap, count);
+        playGameLeft(gameMap, count);
+        playGameRight(gameMap, count);
 
 
     }
 
-    public void playGameUp(int[][] gameMap, boolean[][] hasNum, int count) {
+    public void playGameUp(int[][] gameMap, int count) {
 
         int[][] newMap = new int[N][N];
-        boolean[][] newHasNum = new boolean[N][N];
 
         for(int i=0; i<N; i++) {
             newMap[i] = gameMap[i].clone();
-        }
-
-        for(int i=0; i<N; i++) {
-            newHasNum[i] = hasNum[i].clone();
         }
 
         Queue<Integer> queue = new LinkedList<>();
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (newHasNum[j][i]) {
+                if (newMap[j][i]!=0) {
                     queue.offer(newMap[j][i]);
                     newMap[j][i] = 0;
-                    newHasNum[j][i] = false;
                 }
             }
             int idx = 0;
             while (!queue.isEmpty()) {
                 newMap[idx][i] = queue.poll();
-                newHasNum[idx][i] = true;
                 if (!queue.isEmpty() && newMap[idx][i] == queue.peek()) {
                     newMap[idx][i] += queue.poll();
                 }
                 idx++;
             }
         }
-        playGame(newMap, newHasNum, count+1);
+        playGame(newMap,count+1);
     }
-    public void playGameDown(int[][] gameMap, boolean[][] hasNum, int count) {
+    public void playGameDown(int[][] gameMap,  int count) {
 
         int[][] newMap = new int[N][N];
-        boolean[][] newHasNum = new boolean[N][N];
 
         for(int i=0; i<N; i++) {
             newMap[i] = gameMap[i].clone();
-        }
-
-        for(int i=0; i<N; i++) {
-            newHasNum[i] = hasNum[i].clone();
         }
 
         Queue<Integer> queue = new LinkedList<>();
 
         for (int i = 0; i < N; i++) {
             for (int j = N - 1; j >= 0; j--) {
-                if (newHasNum[j][i]) {
+                if (newMap[j][i]!=0) {
                     queue.offer(newMap[j][i]);
                     newMap[j][i] = 0;
-                    newHasNum[j][i] = false;
                 }
             }
             int idx = N - 1;
             while (!queue.isEmpty()) {
                 newMap[idx][i] = queue.poll();
-                newHasNum[idx][i] = true;
                 if (!queue.isEmpty() && newMap[idx][i] == queue.peek()) {
                     newMap[idx][i] += queue.poll();
                 }
                 idx--;
             }
         }
-        playGame(newMap, newHasNum, count+1);
+        playGame(newMap, count+1);
     }
-    public void playGameLeft(int[][] gameMap, boolean[][] hasNum, int count) {
+    public void playGameLeft(int[][] gameMap, int count) {
 
         int[][] newMap = new int[N][N];
-        boolean[][] newHasNum = new boolean[N][N];
 
         for(int i=0; i<N; i++) {
             newMap[i] = gameMap[i].clone();
-        }
-
-        for(int i=0; i<N; i++) {
-            newHasNum[i] = hasNum[i].clone();
         }
 
         Queue<Integer> queue = new LinkedList<>();
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (newHasNum[i][j]) {
+                if (newMap[i][j]!=0) {
                     queue.offer(newMap[i][j]);
                     newMap[i][j] = 0;
-                    newHasNum[i][j] = false;
                 }
             }
             int idx = 0;
             while (!queue.isEmpty()) {
                 newMap[i][idx] = queue.poll();
-                newHasNum[i][idx] = true;
                 if (!queue.isEmpty() && newMap[i][idx] == queue.peek()) {
                     newMap[i][idx] += queue.poll();
                 }
                 idx++;
             }
         }
-        playGame(newMap, newHasNum, count+1);
+        playGame(newMap, count+1);
     }
-    public void playGameRight(int[][] gameMap, boolean[][] hasNum, int count) {
+    public void playGameRight(int[][] gameMap, int count) {
 
         int[][] newMap = new int[N][N];
-        boolean[][] newHasNum = new boolean[N][N];
 
         for(int i=0; i<N; i++) {
             newMap[i] = gameMap[i].clone();
-        }
-
-        for(int i=0; i<N; i++) {
-            newHasNum[i] = hasNum[i].clone();
         }
 
         Queue<Integer> queue = new LinkedList<>();
 
         for (int i = 0; i < N; i++) {
             for (int j = N - 1; j >= 0; j--) {
-                if (newHasNum[i][j]) {
+                if (newMap[i][j]!=0) {
                     queue.offer(newMap[i][j]);
                     newMap[i][j] = 0;
-                    newHasNum[i][j] = false;
                 }
             }
             int idx = N - 1;
             while (!queue.isEmpty()) {
                 newMap[i][idx] = queue.poll();
-                newHasNum[i][idx] = true;
                 if (!queue.isEmpty() && newMap[i][idx] == queue.peek()) {
                     newMap[i][idx] += queue.poll();
                 }
                 idx--;
             }
         }
-        playGame(newMap, newHasNum, count+1);
+        playGame(newMap, count+1);
     }
 
-    public void calculationScore(int[][] newMap, boolean[][] newHasNum) {
+    public void calculationScore(int[][] newMap) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (newHasNum[i][j]) {
+                if (newMap[i][j]!=0) {
                     maxNum = Math.max(newMap[i][j], maxNum);
                 }
             }
