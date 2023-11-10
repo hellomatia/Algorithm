29b666f6fd7ff7571210d7a3dac889cf60fb85de
hellomatia <101452054+hellomatia@users.noreply.github.com>
2023-11-10 14:59:02 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 229 ms, Memory: 28,812 KB -BaekjoonHub

---
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/README.md
@@ -0,0 +1,15 @@
+# [D4] [S/W 문제해결 응용] 4일차 - 보급로 - 1249 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV15QRX6APsCFAYD) 
+
+### 성능 요약
+
+메모리: 28,812 KB, 시간: 229 ms, 코드길이: 2,260 Bytes
+
+### 제출 일자
+
+2023-11-10 14:58
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -0,0 +1,96 @@
+
+import java.io.*;
+import java.util.*;
+
+class Point {
+	int x;
+	int y;
+	int time;
+	
+	Point(int x, int y, int time) {
+		this.x = x;
+		this.y = y;
+		this.time = time;
+	}
+	
+}
+
+public class Solution {
+	
+	private static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	private static int T;
+	private static int[][] map;
+	private static boolean[][] visited;
+	private static int minTime;
+	private static int[] dirX = {0 ,0 , 1, -1};
+	private static int[] dirY = {1, -1, 0, 0};
+	
+	public static void main(String args[]) throws Exception {
+		solution();
+	}
+	
+	private static void solution() throws IOException {
+		initT();
+		for (int testCase = 1; testCase <= T; testCase++) {
+			initMap();
+			printResult(testCase, minTime());
+		}
+		bw.flush();
+		bw.close();
+	}
+	
+	private static void initT() throws IOException {
+		T = Integer.parseInt(bf.readLine());
+	}
+
+	private static void initMap() throws IOException {
+		int length = Integer.parseInt(bf.readLine());
+		map = new int[length][length];
+		visited = new boolean[length][length];
+		
+		for (int row = 0; row < length; row++) {
+			String string = bf.readLine();
+			for (int col = 0; col < length; col++) {
+				map[row][col] = string.charAt(col) - '0';
+			}
+		}
+	}
+	
+	private static int minTime() {
+		PriorityQueue<Point> queue = new PriorityQueue<>((o1, o2) -> {
+			return o1.time - o2.time;
+		});
+		queue.offer(new Point(0, 0, map[0][0]));
+		minTime = Integer.MAX_VALUE;
+		visited[0][0] = true;
+		
+		while (!queue.isEmpty() ) {
+			Point now = queue.poll();
+						
+			if (now.x == map.length - 1 && now.y == map.length - 1) {
+				minTime = Math.min(minTime, now.time);
+				continue;
+			}
+			
+			for (int i = 0; i < 4; i++) {
+				int nextX = now.x + dirX[i];
+				int nextY = now.y + dirY[i];
+				
+				if (nextX < 0 || nextY < 0 || map.length <= nextX || map.length <= nextY 
+						|| visited[nextX][nextY]) {
+					continue;
+				}
+				visited[nextX][nextY] = true;
+				queue.offer(new Point(nextX, nextY, now.time + map[nextX][nextY]));
+			}
+		}
+		
+		
+		return minTime;
+	}
+	
+	private static void printResult(int testCase, int result) throws IOException {
+		bw.write("#"+testCase+" "+result + "\n");
+	}
+}
