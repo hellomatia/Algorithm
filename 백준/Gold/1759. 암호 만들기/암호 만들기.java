532246f1038f96b90d640cf53fded4765031ab74
hellomatia <101452054+hellomatia@users.noreply.github.com>
2024-02-26 22:17:44 +0900

    [Gold V] Title: 암호 만들기, Time: 92 ms, Memory: 12580 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,101 +1,92 @@
+
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-    ArrayList<Character> vowel = new ArrayList<>();
-    ArrayList<Character> consonant = new ArrayList<>();
-    ArrayList<String> codeList = new ArrayList<>();
 
-    int L, C;
-    public void solution() throws IOException {
+    private BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    private BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private int L, C;
+    private List<Character> jChs;
+    private List<Character> mChs;
+    private PriorityQueue<String> pq;
+    private char[] word;
 
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        L = Integer.parseInt(st.nextToken()); // 만들어야 하는 암호 글자 수
-        C = Integer.parseInt(st.nextToken()); // 알파벳 개수
-
-
-        st = new StringTokenizer(bf.readLine());
-
-        for(int i=0; i<C; i++) {
-            String str = st.nextToken();
-            char ch = str.charAt(0);
-            //모음일 경우 vowel, 자음일 경우 consonant
-            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
-                vowel.add(ch);
-            } else {
-                consonant.add(ch);
-            }
-        }
-
-        StringBuilder code = new StringBuilder();
-
-        makeCodeVowelPart(code, 0, 0);
-
-        Collections.sort(codeList);
-
-        for(int i=0; i<codeList.size(); i++) {
-            bw.write(codeList.get(i));
-            bw.write("\n");
-        }
-        
-
-
-        bw.flush();
-        bw.close();
-    }
-
-    public void makeCodeVowelPart(StringBuilder code, int idx, int count) {
-        if(code.length()>=L-1){
-            return;
-        }
-
-        //System.out.println(1);
-
-        if(0<count && count<L-1) {
-            makeCodeConsonantPart(code, 0, 0);
-        }
-
-        for(int i=idx; i<vowel.size(); i++) {
-            code.append(vowel.get(i));
-            makeCodeVowelPart(code, i+1, count+1);
-            code.deleteCharAt(code.length()-1);
-        }
-    }
-
-    public void makeCodeConsonantPart(StringBuilder code, int idx, int count) {
-        if(code.length()>L) {
-            return;
-        }
-
-        if(code.length()==L) {
-            sortCode(code);
-        }
-
-        for(int i=idx; i<consonant.size(); i++) {
-            code.append(consonant.get(i));
-            makeCodeConsonantPart(code, i+1, count+1);
-            code.deleteCharAt(code.length()-1);
-        }
-    }
-
-    public void sortCode(StringBuilder code) {
-
-        char[] chrArr = code.toString().toCharArray();
-
-        Arrays.sort(chrArr);
-
-        String str = new String(chrArr);
-
-        codeList.add(str);
-    }
-
-    public static void main (String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         new Main().solution();
     }
 
+    private void solution() throws IOException {
+        init();
+        calcResult();
+        printResult();
+        bw.close();
+    }
 
+    private void init() throws IOException {
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        jChs = new ArrayList<>();
+        mChs = new ArrayList<>();
+
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < C; i++) {
+            char ch = st.nextToken().charAt(0);
+            if (isJ(ch)) jChs.add(ch);
+            if (isM(ch)) mChs.add(ch);
+        }
+    }
+
+    private void calcResult() {
+        pq = new PriorityQueue<>();
+        word = new char[L];
+        pickJ(0, 0);
+    }
+
+    private void pickJ(int count, int start) {
+        if (count == L) {
+            return;
+        }
+
+        if (2 <= count) {
+            pickM(count, 0);
+        }
+
+        for (int i = start; i < jChs.size(); i++) {
+            word[count] = jChs.get(i);
+            pickJ(count + 1, i + 1);
+        }
+    }
+
+    private void pickM(int count, int start) {
+        if (count == L) {
+            char[] temp = word.clone();
+            Arrays.sort(temp);
+            pq.offer(String.valueOf(temp));
+            return;
+        }
+
+        for (int i = start; i < mChs.size(); i++) {
+            word[count] = mChs.get(i);
+            pickM(count + 1, i + 1);
+        }
+    }
+
+    private boolean isJ(char ch) {
+        return !isM(ch);
+    }
+
+    private boolean isM(char ch) {
+        return ch == 'a' || ch == 'e' ||ch == 'i' || ch == 'o' || ch == 'u';
+    }
+
+    private void printResult() throws IOException {
+        while (!pq.isEmpty()) {
+            bw.write(pq.poll() + "\n");
+        }
+        bw.flush();
+    }
 }
