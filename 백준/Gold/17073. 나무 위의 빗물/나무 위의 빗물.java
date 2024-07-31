644e52e0abc079bb86d3db108001b62bfdb8c82d
이지표 <101452054+hellomatia@users.noreply.github.com>
2024-07-31 18:35:24 +0900

    [Gold V] Title: 나무 위의 빗물, Time: 2688 ms, Memory: 233532 KB -BaekjoonHub

---
+++ 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private int N, W;
+    private List<Integer>[] adj;
+    private int leaf;
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    private void solution() throws IOException {
+        init();
+        printAns(calcAns());
+    }
+
+    private void init() throws IOException {
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+        adj = new List[N + 1];
+        for (int i = 1; i <= N; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int U = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+            adj[U].add(V);
+            adj[V].add(U);
+        }
+    }
+
+    private String calcAns() {
+        dfs(1, 1);
+        if (adj[1].size() == 1) {
+            leaf--;
+        } else if (adj[1].size() == 0) {
+            leaf++;
+        }
+
+        return String.format("%.6f",(double) W / leaf);
+    }
+
+    private void dfs(int now, int from) {
+        if (adj[now].size() == 1) {
+            leaf++;
+        }
+        for (int next : adj[now]) {
+            if (from == next) { continue; }
+            dfs(next, now);
+        }
+    }
+
+    private void printAns(String ans) throws IOException {
+        bw.write(ans + "\n");
+        bw.flush();
+    }
+}
